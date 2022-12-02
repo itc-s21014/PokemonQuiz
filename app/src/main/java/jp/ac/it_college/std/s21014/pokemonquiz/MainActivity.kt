cd056f365e1e3ef2b4a5d341fc63c4a0f30b3036
s21014 <soccer0907>
2022-12-02 17:40:11 +0900

    完成

--- app/src/main/java/jp/ac/it_college/std/s21014/pokemonquiz/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21014/pokemonquiz/MainActivity.kt
@@ -2,10 +2,18 @@ package jp.ac.it_college.std.s21014.pokemonquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s21014.pokemonquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        title = getString(R.string.app_name)
+        setContentView(binding.root)
+        initJsonData(assets)
+    }
+
+    override fun onBackPressed() {
     }
 }
